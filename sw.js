16d12fe83117d4d6b362d354ad1fcd93bd968564
ma303300-blip <ma303300@gmail.com>
2026-06-18 01:19:24 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,41 @@
+const CACHE_NAME = 'tehilim-beyahad-v1';
+const ASSETS = [
+  '/tehilim-beyahad/',
+  '/tehilim-beyahad/index.html',
+  '/tehilim-beyahad/manifest.json',
+  '/tehilim-beyahad/icon-192.png',
+  '/tehilim-beyahad/icon-512.png',
+  '/tehilim-beyahad/apple-touch-icon.png'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(ASSETS))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))
+      )
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request)
+      .then(cached => cached || fetch(e.request).then(response => {
+        if (response && response.status === 200) {
+          const clone = response.clone();
+          caches.open(CACHE_NAME).then(cache => cache.put(e.request, clone));
+        }
+        return response;
+      }))
+      .catch(() => caches.match('/tehilim-beyahad/index.html'))
+  );
+});
